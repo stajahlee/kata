287889376a09389d8c2ab1bf9dce9efe892ad21a
Stajah Lee Hoeflich <stajah@stajahlee.com>
2021-10-25 09:19:44 -0400

    node project setup

---
+++ tests/index.test.js
@@ -0,0 +1,9 @@
+const { assert } = require("chai");
+const { main } = require("..");
+
+describe("index", () => {
+  it("should run no problem", () => {
+    const result = main();
+    assert.equal(result, 'no problem');
+  });
+});

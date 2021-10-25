5406f7c4ed89c68f41f54572a82bdb627b6b15a6
Stajah Lee Hoeflich <stajah@stajahlee.com>
2021-10-25 13:26:10 -0400

    user input and most basic tests set up for index

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const main = () => 'no problem';
+const inquirer = require('inquirer');
+
+const questions = [];
+
+const getUserInput = async () => inquirer.prompt().then((r) => r);
+
+const main = async () => {
+  const selectedTimes = await getUserInput();
+  return selectedTimes;
+};
 
 main();
 
 module.exports = {
+  questions,
   main,
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,19 @@
 const { assert } = require('chai');
-const { main } = require('..');
+const { main, questions } = require('..');
 
 describe('index', () => {
-  it('should run no problem', () => {
-    const result = main();
-    assert.equal(result, 'no problem');
+  describe('#main', () => {
+    it('should return a number for the babysitting rate for 1 night of work', () => {
+      // 1 night rate represents dollars for full hours of work
+      // does not use fractional hour so no cents will need to be accounted for
+      const result = main();
+      assert.typeOf(result, 'number');
+    });
+  });
+
+  describe('#questions to get startTime, bedTime, and endTime', () => {
+    it('should have three questions', () => {
+      assert.lengthOf(questions, 3);
+    });
   });
 });

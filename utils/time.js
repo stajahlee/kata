b4a1a86c5053f15a4c8345d7bea5f65776eb0859
Stajah Lee Hoeflich <stajah@stajahlee.com>
2021-10-25 13:03:01 -0400

    times populating and tests pass

--- utils/time.js
+++ utils/time.js
@@ -3,9 +3,16 @@
  * @returns object with 3 keys, startTimes, bedTimes, endTimes which give a string array that make sense for that key, eg: startTimes: ['5 PM', '6 PM', ...]
  */
 const getRelevantTimes = () => {
-  const startTimes = [];
-  const bedTimes = [];
-  const endTimes = [];
+  const allPossibleTimes = [];
+  for (let i = 5; i <= 12; i += 1) {
+    allPossibleTimes.push(`${i} ${i === 12 ? 'AM' : 'PM'}`);
+  }
+  for (let j = 1; j <= 4; j += 1) {
+    allPossibleTimes.push(`${j} AM`);
+  }
+  const startTimes = allPossibleTimes.slice(0, -1);
+  const bedTimes = allPossibleTimes.slice(0, -5);
+  const endTimes = allPossibleTimes.slice(1);
   return { startTimes, bedTimes, endTimes };
 };
 
